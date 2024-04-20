6f4e4f6ed6bf23fc77d67092217b7e195cf5738f
RedSLeo <86766332+RedSLeo@users.noreply.github.com>
2024-04-19 22:46:33 -0400

    Test Code
    Simply just a test code for a final project, has nothing to do with the actual project

---
+++ BASICTEST.java
@@ -0,0 +1,10 @@
+class BASICTEST{
+    public static void main(String[] args){
+
+        for (int i = 0; i < 101; i++){
+            System.out.println("Hello World!");
+
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Special Dishes
  Grant the user the ability to choose categorized meals and view ingrendients
+ 
+ Testing Git and GitHUB for CIS111B
